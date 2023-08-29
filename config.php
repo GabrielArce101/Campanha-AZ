5898255cd28224810d87e7c3d71d9f3ea7ed422a
Gabriel Arce Souza <117094290+GabrielArce101@users.noreply.github.com>
2023-08-29 11:09:31 -0400

    config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+$dbHost = 'localhost';
+$dbName = 'AZMerit';// Mudar para AZMerit !!
+$dbUser = 'postgres';
+$dbPass = '1234';
+
+try{
+    $conexao = new PDO("pgsql:host=$dbHost;dbname=" . $dbName, $dbUser, $dbPass);
+    echo "conexao relizada com sucesso";
+}catch(PDOException $err){
+    echo "banco não conectado" . " " .$err->getMessage();
+}
