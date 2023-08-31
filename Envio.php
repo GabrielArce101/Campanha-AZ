68c47fc4a13a3e405e4303eacc914c678cd09c4b
Gabriel Arce Souza <117094290+GabrielArce101@users.noreply.github.com>
2023-08-31 07:58:07 -0400

    Envio.php

--- Envio.php
+++ Envio.php
@@ -1,8 +1,8 @@
 <?php    
-    require_once 'config.php';   
-    $usuario = new Usuario();
+    require_once 'config.php'; 
+    include_once 'campanha.php'; 
+    $Usuario = new Usuario();
 ?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -14,16 +14,15 @@
 </head>
 <body class="home">
     <?php include_once 'menu.php'; ?>
- 
     <div class="tudo">
         <div class="painel-principal">
-
             <img class="moeda" src="img/0099.png" alt="">
             <h1 class="multiplicador">Total: 99999</h1>
             <img class="boneco" src="img/Boneco.png" alt="Carregando...">
 
             <div class="caixa-input">
-                <input required="" name="nome_campanha" id="input-nome" type="text" class="input">
+            
+                <input required="" name="nome_campanha" id="nome_campanha" type="text" class="input">
                 <label class="label">
                     <span class="label-letra" style="--index: 0">N</span>
                     <span class="label-letra" style="--index: 1">o</span>
@@ -50,7 +49,7 @@
             <hr class="linha1">
 
             <div class="caixa-input2">
-                <input required="" type="text" class="input" name="Quantidade_AZMerits" id="input-quantidade">
+                <input required="" type="text" class="input" name="qde_az_por_colaborador" id="qde_az_por_colaborador">
                 <label class="label">
                     <span class="label-letra" style="--index: 1">A</span>
                     <span class="label-letra" style="--index: 2">Z</span>
@@ -84,17 +83,19 @@
             <div class="posicao-data">
                 <form class="data">
                     <label for="data" class="lb">Data de início:</label>
-                    <input name="data_inicio" id="data" type="date" class="infos">       
+                    <input name="data_inicio" id="data_inicio" type="date" class="infos data">
+                    <input class="infos hora" type="time" name="hora_inicio" id="hora_inicio">       
                 </form>
                 <form class="data">
                     <label for="data" class="lb">Data de término:</label>
-                    <input name="data_fim" id="data" type="date" class="infos">       
+                    <input name="data_final" id="data_final" type="date" class="infos data">
+                    <input name="hora_final" id="hora_final" type="time" class="infos hora">
                 </form>
             </div>
 
             <div class="botoes">                  
-                <button type="submit" name="iniciar" id="interior-botao" class="custom-btn btn-1"><a href=Envio.php class="letra-botao" method="POST">Iniciar Campanha</button>
-                <button type="submit" name="cancelar" id="interior-botao" class="custom-btn btn-1"><a class="letra-botao">Cancelar</button>
+                <button type="submit" name="iniciar" id="letra-botao"  class="custom-btn btn-1" onclick="window.location.reload()">Iniciar Campanha</button>
+                <button type="submit" name="cancelar" id="letra-botao" class="custom-btn btn-1" onclick="window.location.reload()">Cancelar</button>
                 <!--Modal do editar-->
                 <div id="fade" class="hide"></div>
 
@@ -136,33 +137,30 @@
         </div>            
     </div>
 
-
     <script src="script.js"></script>
     <script src="modal.js"></script>
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-
 </body>
 </html>
 
 <?php
-    if(isset($_POST['nome_campanha']))
+    if(isset($_POST['id_campanha']))
     {
+        $id_campanha = addslashes($_POST['id_campanha']);
         $nome_campanha = addslashes($_POST['nome_campanha']);
         $qde_az_por_colaborador = addslashes($_POST['qde_az_por_colaborador']);
+        $id_carteira = addslashes($_POST['id_carteira']);
         $saldo_distr = addslashes($_POST['saldo_distr']);
         $data_inicio = addslashes($_POST['data_inicio']);
         $hora_inicio = addslashes($_POST['hora_inicio']);                
         $data_final = addslashes($_POST['data_final']);        
         $hora_final = addslashes($_POST['hora_final']);        
-        if(!empty($nome_campanha)&& !empty($qde_az_por_colaborador)&& !empty($saldo_distr)&& !empty($data_inicio)&& !empty($hora_inicio)&& !empty($data_final)&& !empty($hora_final))
+        if(!empty($id_campanha)&& !empty($nome_campanha)&& !empty($qde_az_por_colaborador)&& !empty($id_carteira)&& !empty($saldo_distr)&& !empty($data_inicio)&& !empty($hora_inicio)&& !empty($data_final)&& !empty($hora_final) )
         {           
-            $Usuario->conectar("AZMerit","localhost","postgres","1234");
-            echo "Conectou";
-            if($Usuario->$msgErro=="") //Deu tudo certo
             {
                 if($data_inicio!=$data_final)
                 {
-                    if($Usuario->campanha($nome_campanha, $qde_az_por_colaborador, $saldo_distr, $data_inicio, $hora_inicio, $data_final, $hora_final))
+                    if($usuario->campanha($id_campanha, $nome_campanha, $qde_az_por_colaborador, $id_carteira, $saldo_distr, $data_inicio, $hora_inicio, $data_final, $hora_final))
                     {
                         ?>
                             <div>
@@ -190,17 +188,7 @@
                     <?php
                 }
             }
-            else
-            {
-                ?>
-                    <div>
-                        <?php echo "Erro:".$Usuario->$msgErro; ?>
-                    </div>
-                <?php
-            }
+
         }
     }
-    
-    ?>
-</body>
-</html>
+?>
